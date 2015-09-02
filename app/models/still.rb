da25bd0054c2ea0b096a8a7ad9c4cdf9172d444f
stearagon <stearagon@gmail.com>
2015-09-02 14:27:27 -0400

    Add Still and Tag class.

---
+++ app/models/still.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: stills
+#
+#  id                 :integer          not null, primary key
+#  name               :string           not null
+#  created_at         :datetime
+#  updated_at         :datetime
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#
+
+class Still < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+
+  
+end
---
+++ app/models/tag.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :integer          not null, primary key
+#  tag        :string           not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Tag < ActiveRecord::Base
+  validates :tag, presence: true, uniqueness: true
+  
+end
