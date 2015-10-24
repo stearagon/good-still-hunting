044358226fb4750b62eea983029c1074ba2d41a3
stearagon <stearagon@gmail.com>
2015-10-24 13:50:37 -0400

    add nested route for still show.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -7,8 +7,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('movies', function(){});
-  this.route('stills', function(){});
-  this.route('still', { path: '/still/:still_id' });
+  this.route('stills', function(){
+    this.route('still', { path: '/:still_id' }, function(){});
+  });
 });
 
 export default Router;
