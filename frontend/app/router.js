343bfd610561f4048bb05270aeeb71d7d7244d59
stearagon <stearagon@gmail.com>
2015-10-24 14:10:10 -0400

    Change movie name to film.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('movies', function(){});
+  this.route('films', function(){});
   this.route('stills', function(){
     this.route('still', { path: '/:still_id' }, function(){});
   });
