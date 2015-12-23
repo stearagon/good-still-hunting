cd049b796e2b3bd140f92ad99246b2c8f113b740
stearagon <stearagon@gmail.com>
2015-12-22 23:36:03 -0500

    Chnage where ember app is rendered.'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  mount_ember_app :frontend, to: "/"
   namespace :api do
     resources :stills
     resources :movies
     resources :stills_tags
     resources :tags, only: [:show, :create, :index]
   end
+
+  mount_ember_app :frontend, to: "/"
 end
