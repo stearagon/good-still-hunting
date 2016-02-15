088924006cdbbce42ab20e8ab8869b4e56b49fdf
stearagon <stearagon@gmail.com>
2016-02-15 15:27:19 -0500

    Add Ember Bootstrap.

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -3,7 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-
+    'ember-bootstrap': {
+      'importBootstrapTheme': true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
