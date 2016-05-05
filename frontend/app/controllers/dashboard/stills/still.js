cc231cd33f326694ca8623fb21e05baf96e8ce54
stearagon <stearagon@gmail.com>
2016-05-04 21:14:33 -0400

    Add initial styling for still show page

--- frontend/app/controllers/dashboard/stills/still.js
+++ frontend/app/controllers/dashboard/stills/still.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  classNames: ['still-show'],
+
   tags: Ember.computed('model.still.tags', function(){
     return this.get('model.still.tags');
   })
