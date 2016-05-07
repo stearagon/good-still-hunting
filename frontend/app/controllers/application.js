e1cf37b40add7649bce4c2850c27e30dc936c7e7
stearagon <stearagon@gmail.com>
2016-05-07 11:43:11 -0400

    Change copy on login form

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -6,6 +6,7 @@ export default Ember.Controller.extend({
   actions: {
     logout() {
       this.get('session').invalidate();
+      this.transitionToRoute('dashboard');
     },
 
     searchStills(searchInput) {
