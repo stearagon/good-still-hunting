cb2bb744a8ca6d78a38dc492526c535a43dd406e
stearagon <stearagon@gmail.com>
2016-05-07 18:40:15 -0400

    Do not transition when logging out

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -6,7 +6,6 @@ export default Ember.Controller.extend({
   actions: {
     logout() {
       this.get('session').invalidate();
-      this.transitionToRoute('dashboard');
     },
 
     searchStills(searchInput) {
