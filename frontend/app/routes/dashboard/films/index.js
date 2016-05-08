b97f1681da22246045080b6dd8c92340a0c709df
stearagon <stearagon@gmail.com>
2016-05-08 12:54:50 -0400

    Fix body height consistency for each apge

--- frontend/app/routes/dashboard/films/index.js
+++ frontend/app/routes/dashboard/films/index.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import ResetScroll from 'frontend/mixins/reset-scroll';
 
 const { service } = Ember.inject;
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, ResetScroll, {
   session: service('session'),
-  
+
   model() {
     return this.store.findAll('movie');
   }
