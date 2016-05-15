676c64b90981b5e7879229d98d789b33222df5c1
stearagon <stearagon@gmail.com>
2016-05-14 22:29:11 -0400

    Sort films index by title ascending

--- frontend/app/controllers/dashboard/films/index.js
+++ frontend/app/controllers/dashboard/films/index.js
@@ -2,4 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   classNames: ['films-list'],
+
+  filmSort: ['title:asc'],
+
+  films: Ember.computed.sort('model', 'filmSort'),
 });
