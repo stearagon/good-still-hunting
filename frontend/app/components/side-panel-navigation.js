26c772c09990d2428270eb62498c1d6fa4a25443
stearagon <me@stevearagon.io>
2016-08-20 02:33:29 -0400

    move sidepanel to dropdown

--- frontend/app/components/side-panel-navigation.js
+++ frontend/app/components/side-panel-navigation.js
@@ -1,28 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didRender() {
-    this._super(...arguments);
+    didRender() {
+        this._super(...arguments);
 
-    if (Ember.$(document).attr('loadSidePanel') === undefined) {
-      Ember.$(document).ready(() => {
-        Ember.$(document).mouseup((e) => {
-          var container = $('.page-side-panel');
-          var hamburger = $('.fa.fa-bars');
+        if (Ember.$(document).attr('loadSidePanel') === undefined) {
+            Ember.$(document).ready(() => {
+                Ember.$(document).mouseup((e) => {
+                    var container = $('.page-side-panel');
+                    var hamburger = $('.fa.fa-bars');
 
-          if (!container.is(e.target) && !hamburger.is(e.target) && container.has(e.target).length === 0) {
-            this.attrs.closeSidePanel();
-          }
-        })
-      });
+                    if (!container.is(e.target) && !hamburger.is(e.target) && container.has(e.target).length === 0) {
+                        this.attrs.closeSidePanel();
+                    }
+                })
+            });
+        }
+
+        Ember.$(document).attr('loadSidePanel', true);
+    },
+
+    actions: {
+        toggleSidePanel(route) {
+            this.attrs.toggleSidePanel(route);
+        }
     }
-
-    Ember.$(document).attr('loadSidePanel', true);
-  },
-
-  actions: {
-    toggleSidePanel() {
-      this.attrs.toggleSidePanel();
-    }
-  }
 });
--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -1,31 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service('session'),
+    session: Ember.inject.service('session'),
 
-  sidePanelOpen: false,
+    sidePanelOpen: false,
 
-  displaySidePanel: Ember.computed('sidePanelOpen', function(){
-    return this.get('sidePanelOpen') ? 'display-side-panel' : '';
-  }),
+    displaySidePanel: Ember.computed('sidePanelOpen', function(){
+        return this.get('sidePanelOpen') ? 'display-side-panel' : '';
+    }),
 
-  actions: {
-    logout() {
-      this.get('session').invalidate();
+    actions: {
+        logout() {
+            this.get('session').invalidate();
+        },
+
+        searchStills(searchInput) {
+            let queryParams = { page: 1, searchInput: searchInput };
+
+            this.transitionToRoute('dashboard.stills.search', { queryParams: queryParams });
+        },
+
+        openSidePanel(route) {
+            this.transitionToRoute(route);
+            this.toggleProperty('sidePanelOpen');
+        },
+
+        closeSidePanel() {
+            this.set('sidePanelOpen', false);
+        },
     },
-
-    searchStills(searchInput) {
-      let queryParams = { page: 1, searchInput: searchInput };
-
-      this.transitionToRoute('dashboard.stills.search', { queryParams: queryParams });
-    },
-
-    openSidePanel() {
-      this.toggleProperty('sidePanelOpen');
-    },
-
-    closeSidePanel() {
-      this.set('sidePanelOpen', false);
-    },
-  },
 });
