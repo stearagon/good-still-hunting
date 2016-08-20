af841e692eb1975a9f76e78547cce9e1449473ff
stearagon <me@stevearagon.io>
2016-08-20 14:02:39 -0400

    Add titles back to all still list pages

--- frontend/app/components/stills-list.js
+++ frontend/app/components/stills-list.js
@@ -2,108 +2,114 @@ import Ember from 'ember';
 import _ from 'lodash/lodash';
 
 const metaFields = [
-  'searchInput',
-  'page',
-  'perPage',
-  'movieTitle',
+    'searchInput',
+    'page',
+    'perPage',
+    'movieTitle',
 ];
 
 export default Ember.Component.extend({
-  movieTitle: null,
-  isLoading: 0,
-  meta: null,
-  page: null,
-  perPage: null,
-  stills: null,
-  tag: null,
+    isLoading: 0,
+    meta: null,
+    page: null,
+    perPage: null,
+    stills: null,
+    tag: null,
 
-  stillSoloSelected: null,
-  stillSoloSelectedMovie: null,
-  stillSoloSelectedTags: null,
+    stillSoloSelected: null,
+    stillSoloSelectedMovie: null,
+    stillSoloSelectedTags: null,
 
-  stillFormOpen: false,
+    displayTitle: Ember.computed('movieTitle', 'searchInput', function() {
+        return this.get('movieTitle') ||
+            this.get('searchInput') ||
+            this.get('tag.tag') ||
+            null;
+    }),
 
-  endOfList: Ember.computed('meta', 'page', 'isLoading', function() {
-    return this.get('meta').total_pages === this.get('page') && this.get('isLoading') === 0;
-  }),
+    stillFormOpen: false,
 
-  didInsertElement() {
-    this._super(...arguments);
+    endOfList: Ember.computed('meta', 'page', 'isLoading', function() {
+        return this.get('meta').total_pages === this.get('page') && this.get('isLoading') === 0;
+    }),
 
-    Ember.$(document).ready(function(){
-      Ember.$('body').height(Ember.$(document).height() + 1);
-      Ember.$(window).scroll(function() {
-        if(Ember.$(window).height() + Ember.$(window).scrollTop() === Ember.$(document).height()) {
-          if(Ember.$('.load-button')){
-            Ember.$('.load-button').click();
-          }
-        }
-      });
+    didInsertElement() {
+        this._super(...arguments);
 
-      Ember.$(window).on('touchstart', function() {
-        if(Ember.$(window).height() + Ember.$(window).scrollTop() === Ember.$(document).height()) {
-          if(Ember.$('.load-button')){
-            Ember.$('.load-button').click();
-          }
-        }
-      });
-    });
-  },
+        Ember.$(document).ready(function(){
+            Ember.$('body').height(Ember.$(document).height() + 1);
+            Ember.$(window).scroll(function() {
+                if(Ember.$(window).height() + Ember.$(window).scrollTop() === Ember.$(document).height()) {
+                    if(Ember.$('.load-button')){
+                        Ember.$('.load-button').click();
+                    }
+                }
+            });
 
-  willDestroyElement() {
-    this._super(...arguments);
-
-    Ember.$('body').height(Ember.$(document).height() - 1);
-    Ember.$(window).off();
-  },
-
-  actions: {
-    openStillSoloModal(still) {
-      if(!this.get('stillFormOpen')) {
-        this.toggleProperty('stillFormOpen');
-      }
-
-      this.set('stillSoloSelected', still);
+            Ember.$(window).on('touchstart', function() {
+                if(Ember.$(window).height() + Ember.$(window).scrollTop() === Ember.$(document).height()) {
+                    if(Ember.$('.load-button')){
+                        Ember.$('.load-button').click();
+                    }
+                }
+            });
+        });
     },
 
-    closeStillSoloModal() {
-      this.toggleProperty('stillFormOpen');
+    willDestroyElement() {
+        this._super(...arguments);
+
+        Ember.$('body').height(Ember.$(document).height() - 1);
+        Ember.$(window).off();
     },
 
-    onLoadNext() {
-      var params = {};
+    actions: {
+        openStillSoloModal(still) {
+            if(!this.get('stillFormOpen')) {
+                this.toggleProperty('stillFormOpen');
+            }
 
-      this.set('loading', true);
+            this.set('stillSoloSelected', still);
+        },
 
-      if (this.get('meta').total_pages > this.get('page')) {
-        this.set('isLoading', this.get('isLoading') + 1);
-        this.set('page', parseInt(this.get('page')) + 1);
+        closeStillSoloModal() {
+            this.toggleProperty('stillFormOpen');
+        },
 
-        metaFields.forEach((field) => {
-          let value = this.get(field);
-          let underscore = Ember.String.underscore(field);
-          let argumentObject = {};
+        onLoadNext() {
+            var params = {};
 
-          argumentObject[underscore] = value;
+            this.set('loading', true);
 
-          if(value) {
-            _.extend(params, argumentObject);
-          }
-        });
+            if (this.get('meta').total_pages > this.get('page')) {
+                this.set('isLoading', this.get('isLoading') + 1);
+                this.set('page', parseInt(this.get('page')) + 1);
 
-        if (this.get('tag')) {
-          _.extend(params, { tag_id: this.get('tag.id') });
+                metaFields.forEach((field) => {
+                    let value = this.get(field);
+                    let underscore = Ember.String.underscore(field);
+                    let argumentObject = {};
+
+                    argumentObject[underscore] = value;
+
+                    if(value) {
+                        _.extend(params, argumentObject);
+                    }
+                });
+
+                if (this.get('tag')) {
+                    _.extend(params, { tag_id: this.get('tag.id') });
+                }
+
+                this.get('store').query('still', params).then((model) => {
+                    this.set('stills', this.get('stills').toArray().addObjects(model.toArray()));
+                    this.set('meta', model.meta);
+
+                    Ember.run.later(() => {
+                        this.set('isLoading', this.get('isLoading') - 1);
+                    }, 1000);
+                });
+            }
         }
-
-        this.get('store').query('still', params).then((model) => {
-          this.set('stills', this.get('stills').toArray().addObjects(model.toArray()));
-          this.set('meta', model.meta);
-
-          Ember.run.later(() => {
-              this.set('isLoading', this.get('isLoading') - 1);
-          }, 1000);
-        });
-      }
     }
-  }
 });
